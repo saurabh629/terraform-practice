e997cf38122b2fa3c89d4a889d6c34fdf51b46fb
Saurabhkumar Parmar <saurabh@Sunbook.local>
2019-07-30 11:58:24 -0400

    Initial Commit

---
+++ Onica-test/main.tf
@@ -0,0 +1,28 @@
+########################################################################
+# Root Module / main.tf
+########################################################################
+
+# Deploy AWS provider
+provider "aws" {
+  region    =   "${var.aws_region}"
+}
+# Deploy Networking Resources
+module "networking" {
+  source        = "./networking"
+  vpc_cidr      = "${var.vpc_cidr}"
+  public_cidrs  = "${var.public_cidrs}"
+  accessip      = "${var.accessip}"
+}
+# Deploy Compute Resources
+module "compute" {
+  source          = "./compute"
+  key_name        = "${var.key_name}" 
+  public_key_path = "${var.public_key_path}"
+  instance_type   = "${var.server_instance_type}"
+  //instance_count  = "${var.instance_count}"
+  subnets         = "${module.networking.public_subnets}"
+  security_groups = "${module.networking.public_sg}"
+  subnet_ips      = "${module.networking.subnet_ips}"
+  vpc_id          = "${module.networking.vpc_id}"
+}
+
